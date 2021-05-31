1fe185ad0dd4619bb1743fd48f5d5a1d62dcf860
PawlowskiAdrian <petroniusz2008@gmail.com>
2021-05-31 15:14:35 +0200

    Update script (Adrian Pawlowski)
    Description:
    
      Script updated.

--- scripts/update_plugins.sh
+++ scripts/update_plugins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PLUGIN_SERVER_IMAGE=$(/usr/local/bin/docker-compose ps -q plugin_server)
-
+PLUGIN_SERVER_IMAGE="$(/usr/local/bin/docker-compose ps -q plugin_server)"
+export PLUGIN_SERVER_IMAGE
 # echo "Removing existing plugin folder in Docker container.."
 # docker-compose exec plugin_server rm -rf /app/plugins/
 echo "Copying current plugin folder to Docker container.."
-docker cp ../server/plugins/* "${PLUGIN_SERVER_IMAGE}":/app/plugins/
+docker cp ./server/plugins/ "${PLUGIN_SERVER_IMAGE}":/app/
 echo "Done."
